a2ceff7fe23968951b94aa70a3a2c11ba0bb317b
ali-suliman <ali.souliman.dev@gmail.com>
2021-01-17 18:27:13 +0100

    Create DiskNavbar.vue

---
+++ src/components/Navigation/DiskNavbar.vue
@@ -0,0 +1,119 @@
+<template>
+  <nav id="nav">
+    <img
+      @click="navToHome"
+      src="../../assets/images/Codic-logo.svg"
+      alt=""
+      class="logo"
+    />
+    <section class="links">
+      <router-link to="/">startSidan</router-link>
+      <router-link to="/courses">våra kurser</router-link>
+      <router-link to="/contact">kontakta oss</router-link>
+    </section>
+
+    <section class="reg">
+      <button class="logout" @click="logout">logga ut</button>
+      <router-link class="signup" to="/profile">mina sidor</router-link>
+    </section>
+  </nav>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout: function() {
+      this.$store.dispatch("logout")
+    },
+    navToHome: function() {
+      this.$router.push("/")
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/styles/globalStyles.scss";
+nav {
+  width: 100%;
+  height: 6rem;
+  padding: 2rem 6rem;
+  @include flex();
+  flex-direction: row;
+  justify-content: flex-start;
+
+  img {
+    cursor: pointer;
+  }
+
+  .logo {
+    width: 6.5rem;
+  }
+
+  a {
+    margin: 0rem 1rem;
+    padding: 0.5rem 1rem;
+    color: #888;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 0.85rem;
+    transition: all 0.4s ease;
+  }
+
+  .links {
+    margin: 0rem 6rem;
+
+    a {
+      &.router-link-exact-active {
+        color: $main-color;
+        font-weight: bold;
+        border-bottom: 1.9px solid $main-color;
+      }
+
+      &:hover {
+        color: $main-color;
+      }
+    }
+  }
+
+  .reg {
+    margin-left: auto;
+
+    a,
+    button {
+      font-weight: bold;
+      padding: 0.6rem 1.8rem;
+      transition: all 0.4s ease;
+      border: none;
+      cursor: pointer;
+    }
+
+    .signup {
+      font-size: 0.7rem;
+      color: #f2f2f2;
+      background: #444;
+      border-radius: 2rem;
+      &:hover {
+        color: #aaa;
+        background: #555;
+      }
+    }
+
+    .logout {
+      font-size: 0.8rem;
+      color: #666;
+      background: transparent;
+      text-transform: uppercase;
+      &:hover {
+        color: #aaa;
+      }
+    }
+  }
+}
+
+@media only screen and (max-width: 1250px) {
+  nav {
+    display: none;
+  }
+}
+</style>

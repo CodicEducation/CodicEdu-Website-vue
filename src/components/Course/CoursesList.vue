b017114ee7235bbb3a8319c2a50387aa7b6444bc
ali-suliman <ali.souliman.dev@gmail.com>
2021-01-17 18:26:54 +0100

    Create CoursesList.vue

---
+++ src/components/Course/CoursesList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="courses-list">
+    <course v-for="course in courses" :key="course.name" :course="course" />
+  </div>
+</template>
+
+<script>
+import Course from "./CourseInfo"
+export default {
+  components: {
+    Course,
+  },
+  computed: {
+    courses() {
+      return this.$store.state.courses
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/styles/globalStyles.scss";
+.courses-list {
+  @include flex();
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+
+  p {
+    color: #eee;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .courses-list {
+    width: 100vw;
+    display: flex;
+    flex-wrap: nowrap;
+    overflow-x: auto;
+    align-items: flex-start;
+    justify-content: flex-start;
+    align-self: flex-start;
+  }
+}
+</style>

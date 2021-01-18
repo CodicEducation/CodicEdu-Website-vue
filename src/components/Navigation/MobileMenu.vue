d162e6c23a2d42f25ad9b36785fad8e081909b2d
ali-suliman <ali.souliman.dev@gmail.com>
2021-01-18 13:20:35 +0100

    Update MobileMenu.vue

--- src/components/Navigation/MobileMenu.vue
+++ src/components/Navigation/MobileMenu.vue
@@ -1,26 +1,40 @@
 <template>
-  <div class="mobile-menu">
-    <button><img src="../../assets/images/closeBtn.svg" alt="" /></button>
-    <ul>
-      <li>
-        <router-link to="/">startsidan</router-link>
-      </li>
-      <li>
-        <router-link to="/courses">våra kurser</router-link>
-      </li>
-      <li>
-        <router-link to="/contact">kontakta oss</router-link>
-      </li>
-    </ul>
-  </div>
+  <transition name="slidIn" mode="in-out">
+    <div class="mobile-menu">
+      <button class="closeBtn">
+        <img src="../../assets/images/closeBtn.svg" alt="" @click="hideMenu" />
+      </button>
+      <ul>
+        <li>
+          <router-link to="/">startsidan</router-link>
+        </li>
+        <li>
+          <router-link to="/courses">våra kurser</router-link>
+        </li>
+        <li>
+          <router-link to="/contact">kontakta oss</router-link>
+        </li>
+      </ul>
+    </div>
+  </transition>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    hideMenu: function() {
+      this.$emit("hideMenu")
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/styles/globalStyles.scss";
 .mobile-menu {
+  @include flex();
+  justify-content: flex-start;
+  padding: 2.3rem 0rem;
   position: fixed;
   top: 0;
   right: 0;
@@ -29,5 +43,16 @@ export default {}
   background: #222;
   z-index: 11;
   color: #aaa;
+
+  .closeBtn {
+    align-self: flex-end;
+    margin: 0rem 2.5rem;
+    background: transparent;
+    border: none;
+
+    img {
+      width: 1.2rem;
+    }
+  }
 }
 </style>

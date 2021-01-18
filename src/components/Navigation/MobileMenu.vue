68931b8574bdafdb39eb21d30ccceb03d7d0d2ec
ali-suliman <ali.souliman.dev@gmail.com>
2021-01-18 12:52:24 +0100

    Create MobileMenu.vue

---
+++ src/components/Navigation/MobileMenu.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mobile-menu">
+    <button><img src="../../assets/images/closeBtn.svg" alt="" /></button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.mobile-menu {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100vw;
+  height: 100vh;
+  background: #222;
+  z-index: 11;
+  color: #aaa;
+}
+</style>

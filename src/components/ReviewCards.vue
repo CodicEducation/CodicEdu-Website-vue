d619d27baa921d4cc16a7778bde04be1b73bf4db
ali-suliman <ali.souliman.dev@gmail.com>
2021-01-22 14:00:25 +0100

    Create ReviewCards.vue

---
+++ src/components/ReviewCards.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="rev-card">
+    <p class="rev-txt">
+      {{ review }}
+    </p>
+    <p class="rev-info">{{ reviewer }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    review: String,
+    reviewer: String,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/globalStyles.scss";
+.rev-card {
+  background: #ffffff06;
+  max-width: 44rem;
+  height: 20rem;
+  margin: 2rem;
+  padding: 3rem;
+  @include flex();
+  justify-content: space-between;
+  align-items: flex-start;
+  text-align: left;
+  border-radius: 12px;
+
+  .rev-txt {
+    line-height: 1.8rem;
+    color: #eee;
+  }
+
+  .rev-info {
+    font-size: 0.9rem;
+    color: #999;
+  }
+}
+</style>

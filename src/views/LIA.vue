9d6381ceb3485f781141cdebedf1a78fc654fb11
ali-suliman <ali.souliman.dev@gmail.com>
2021-01-27 16:16:22 +0100

    Create LIA.vue

---
+++ src/views/LIA.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="lia">
+    <disk-nav />
+    <mobile-navbar />
+
+    <main>
+      <h1>This is the lia page</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae possimus
+        laudantium libero qui cupiditate ut ea commodi nam animi repellat!
+      </p>
+    </main>
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+import DiskNav from "../components/Navigation/DiskNavbar.vue"
+import MobileNavbar from "../components/Navigation/MobileNavbar.vue"
+
+import Footer from "../components/Footer/Footer.vue"
+export default {
+  components: {
+    DiskNav,
+    MobileNavbar,
+    Footer,
+  },
+}
+</script>
+
+<style></style>

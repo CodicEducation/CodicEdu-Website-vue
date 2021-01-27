4cb3c24ca47d2675141502c13fe7fb6cc4addeb4
ali-suliman <ali.souliman.dev@gmail.com>
2021-01-27 16:16:24 +0100

    Update Profile.vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,13 +5,22 @@
 
     <main>
       <section class="user-info">
-        <div class="img">
-          <img src="../assets/images/profileImg.svg" alt="" />
+        <div class="content">
+          <div class="img">
+            <img :src="user.imageUrl" alt="" />
+          </div>
+          <h1>{{ user.name }}</h1>
+          <p>
+            {{ user.email }}
+          </p>
+          <input
+            name="file"
+            type="file"
+            accept="image/png, image/jpeg, image/jpg"
+            @change="uploadImg"
+          />
         </div>
-        <h1>{{ user.name }}</h1>
-        <p>
-          {{ user.email }}
-        </p>
+        {{ url }}
       </section>
 
       <section class="list">
@@ -46,8 +55,20 @@ export default {
       return this.$store.state.user
     },
   },
+
   created() {
     this.$store.dispatch("getUser", this.uid)
+  },
+
+  methods: {
+    uploadImg: function(e) {
+      let file = e.target.files[0]
+      let data = {
+        file: file,
+        uid: this.uid,
+      }
+      this.$store.dispatch("uploadImg", data)
+    },
   },
 }
 </script>
@@ -64,42 +85,78 @@ export default {
   main {
     position: relative;
     width: 100%;
-    height: 100%;
+    height: 90vh;
     color: #eee;
     @include flex();
     justify-content: space-between;
+    flex-direction: row;
 
     .user-info {
       width: 50%;
       height: 80vh;
       @include flex();
       justify-content: flex-start;
-
       padding: 2rem 0rem;
 
-      .img {
-        width: 20rem;
-        height: 22rem;
-        margin: 4rem 0rem 2rem 0rem;
+      .content {
+        @include flex();
+        justify-content: flex-start;
+        align-items: flex-start;
 
-        img {
-          width: 100%;
+        .img {
+          width: 18rem;
+          height: 18rem;
+          border-radius: 50%;
+          overflow: hidden;
+          margin: 4rem 0rem 2rem 0rem;
+
+          img {
+            height: 100%;
+          }
         }
-      }
 
-      h1 {
-        margin: 1rem 0rem;
-        text-transform: capitalize;
-        font-size: 1.8rem;
-      }
+        h1 {
+          margin: 2rem 0rem 1rem 0rem;
+          text-transform: capitalize;
+          font-size: 1.8rem;
+        }
 
-      p {
-        color: #afafaf;
+        p {
+          color: #afafaf;
+        }
+
+        input {
+          margin: 2rem 0rem;
+          background: #ddd;
+          height: 2.5rem;
+          position: relative;
+          overflow: hidden;
+          border-radius: 5px;
+          width: 100%;
+
+          &::after {
+            content: "Ladda upp en bild ";
+            padding: 0.8rem 0rem;
+            color: #222;
+            font-weight: bold;
+            text-align: center;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: #fefefe;
+            z-index: 11;
+          }
+        }
       }
     }
 
     .list {
-      width: 100%;
+      width: 45%;
+      height: 100%;
+      overflow-y: scroll;
+      padding: 2rem 0rem 1rem 0rem;
     }
   }
 }
@@ -107,6 +164,9 @@ export default {
 @media only screen and (max-width: 768px) {
   .profile {
     main {
+      height: initial;
+      flex-direction: column;
+
       .user-info {
         width: 100%;
         height: inherit;
@@ -123,6 +183,13 @@ export default {
         p {
           font-size: 0.9rem;
         }
+      }
+
+      .list {
+        width: 100%;
+        height: 100vh;
+        overflow-y: scroll;
+        padding-top: 4rem;
       }
     }
   }

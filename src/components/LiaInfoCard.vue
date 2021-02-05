5e54472414ebf6a9981f7201a187416d7550c573
ali-suliman <ali.souliman.dev@gmail.com>
2021-02-05 01:47:06 +0100

    Create LiaInfoCard.vue

---
+++ src/components/LiaInfoCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="liaInfo">
+    <img :src="url" alt="" />
+    <p>{{ txt }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    url: String,
+    txt: String,
+  },
+}
+</script>
+
+<style></style>

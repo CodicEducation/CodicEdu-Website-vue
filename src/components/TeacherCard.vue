83904bb432fa6ea67d35a0ad31538e14155d2c14
ali-suliman <ali.suliman.alx@gmail.com>
2021-03-03 19:53:34 +0100

    Update TeacherCard.vue

--- src/components/TeacherCard.vue
+++ src/components/TeacherCard.vue
@@ -9,7 +9,11 @@
       <h4>{{ fullName }}</h4>
       <div class="footer">
         <p class="contact-title">Färdigheter</p>
-        <p class="title">{{ title }}</p>
+        <ul>
+          <li v-for="(skill, index) in skills" :key="index">
+            {{ skill }}
+          </li>
+        </ul>
       </div>
     </section>
   </div>
@@ -20,6 +24,7 @@ export default {
   props: {
     fullName: String,
     title: String,
+    skills: Array,
   },
 
   data: () => {
@@ -37,50 +42,53 @@ export default {
   justify-content: space-between;
   background: #ffffff06;
   width: 18rem;
-  height: 26rem;
-  margin: 2rem 1rem;
+  height: 40rem;
+  margin: 4rem 1rem;
   border-radius: 12px;
 
   img {
     max-width: 9rem;
-    margin: 4rem 0rem 1rem 0rem;
+    margin: 4rem 0rem 0rem 0rem;
   }
 
   .contact-info {
     width: 100%;
-    height: 50%;
+    height: 60%;
     @include flex();
-    justify-content: space-between;
+    justify-content: flex-end;
 
     h4 {
-      margin-bottom: 1rem;
-    }
-
-    .contact-title {
-      color: #888;
-      margin-bottom: 0.4rem;
-    }
-
-    p {
-      font-size: 0.9rem;
-      margin: 0.2rem 0rem;
-      color: #e2e2e2;
-      align-self: flex-start;
-      text-align: left;
-
-      &::first-letter {
-        text-transform: uppercase;
-      }
+      margin-bottom: 4rem;
     }
 
     .footer {
       width: 100%;
-      height: 70%;
+      min-height: 80%;
       background: #ffffff06;
       @include flex();
       align-items: flex-start;
       justify-content: flex-start;
       padding: 1.4rem;
+
+      .contact-title {
+        color: #888;
+      }
+
+      ul {
+        height: 100%;
+        @include flex();
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-items: flex-start;
+        justify-content: flex-start;
+        padding: 1rem 0rem;
+
+        li {
+          margin: 0.5rem 1rem;
+          color: #ddd;
+          text-transform: capitalize;
+        }
+      }
     }
   }
 }
@@ -89,4 +97,55 @@ export default {
   .teacher-card {
   }
 }
+
+// .contact-info {
+//   width: 100%;
+//   height: 60%;
+//   @include flex();
+//   justify-content: space-between;
+
+//   h4 {
+//     margin-bottom: 4rem;
+//   }
+
+//   .contact-title {
+//     color: #888;
+//     margin: 1.5rem 0rem;
+//   }
+
+//   p {
+//     font-size: 0.9rem;
+//     margin: 0.2rem 0rem;
+//     color: #e2e2e2;
+//     align-self: flex-start;
+//     text-align: left;
+
+//     &::first-letter {
+//       text-transform: uppercase;
+//     }
+//   }
+
+//   .footer {
+//     width: 100%;
+//     min-height: 100%;
+//     background: #ffffff06;
+//     @include flex();
+//     align-items: flex-start;
+//     justify-content: flex-start;
+//     padding: 1.4rem;
+
+//     ul {
+//       height: 100%;
+//       @include flex();
+//       flex-direction: row;
+//       flex-wrap: wrap;
+//       align-items: flex-start;
+//       justify-content: flex-start;
+
+//       li {
+//         margin: 1rem;
+//       }
+//     }
+//   }
+// }
 </style>
